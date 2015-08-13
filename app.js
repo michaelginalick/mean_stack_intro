2f2d2083dd4bbbf494c3826c2aa8fb23cdb02cb9
Michael <Michael.ginalick@gmail.com>
2015-08-12 22:24:02 -0500

    getting the right post

--- app.js
+++ app.js
@@ -13,16 +13,31 @@ app.factory('posts', [function(){
 app.config([
 '$stateProvider',
 '$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+    function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    });
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        })
+      
+        .state('posts', {
+            url: 'posts/{id}',
+            templateUrl: 'posts.html',
+            controller: 'PostsCtrl'
+      });
 
   $urlRouterProvider.otherwise('home');
+}]);
+
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts) {
+        $scope.post = post.posts[$stateParams.id];
 }]);
 
 
@@ -38,8 +53,13 @@ app.controller('MainCtrl', ['$scope', 'posts', function ($scope, posts) {
        $scope.posts.push({
            title: $scope.title, 
            link: $scope.link,
-           upvotes: 0
+           upvotes: 0,
+           comments: [
+                {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+            ]
        });
+        
        $scope.title = '';
        $scope.link = '';    
     };
