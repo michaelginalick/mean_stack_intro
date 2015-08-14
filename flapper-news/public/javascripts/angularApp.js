7da7dd82d78fce6b9eb503f4c89d7ade7911105d
Michael <Michael.ginalick@gmail.com>
2015-08-13 21:46:14 -0500

    added routes

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -54,23 +54,23 @@ function($scope, posts){
 
 }])
 .controller('PostsCtrl', [
-'$scope',
-'$stateParams',
-'posts',
-function($scope, $stateParams, posts){
-  $scope.post = posts.posts[$stateParams.id];
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+        $scope.post = posts.posts[$stateParams.id];
 
-  $scope.addComment = function(){
-    if($scope.body === '') { return; }
-    $scope.post.comments.push({
-      body: $scope.body,
-      author: 'user',
-      upvotes: 0
-    });
-    $scope.body = '';
-  };
+        $scope.addComment = function(){
+            if($scope.body === '') { return; }
+                $scope.post.comments.push({
+                    body: $scope.body,
+                    author: 'user',
+                    upvotes: 0
+            });
+            $scope.body = '';
+        };
 
-  $scope.incrementUpvotes = function(comment){
-    comment.upvotes += 1;
-  };
+    $scope.incrementUpvotes = function(comment){
+        comment.upvotes += 1;
+    };
 }]);
